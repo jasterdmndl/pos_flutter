3aa3dc39fad688d347595c93c0ec4d678fb0ae3e
jasterdmndl <24-01505@g.batstate-u.edu.ph>
2026-06-23 10:50:05 +0800

    Connects Riverpod to product_page.dart

--- lib/features/products/product_page.dart
+++ lib/features/products/product_page.dart
@@ -1,38 +1,14 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'product_provider.dart';
 import 'product_model.dart';
 
-class ProductPage extends StatelessWidget {
+class ProductPage extends ConsumerWidget {
   const ProductPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final products = [
-      const Product(
-        id: 1,
-        name: 'Latte',
-        price: 120,
-        isActive: true,
-      ),
-      const Product(
-        id: 2,
-        name: 'Mocha',
-        price: 140,
-        isActive: true,
-      ),
-      const Product(
-        id: 3,
-        name: 'Americano',
-        price: 100,
-        isActive: true,
-      ),
-      const Product(
-        id: 4,
-        name: 'Cappuccino',
-        price: 130,
-        isActive: true,
-      ),
-    ];
+  Widget build(BuildContext context, WidgetRef ref) {
+    final products = ref.watch(productProvider);
 
     return Scaffold(
       appBar: AppBar(
